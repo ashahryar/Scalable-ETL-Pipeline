a280a95b13481f89f518377917a3586f33f86635
Shahryar__Ahmed <ashahryar200@gmail.com>
2026-05-08 11:51:35 +0500

    Add files via upload

---
+++ scripts/aws_copy.sh
@@ -0,0 +1,39 @@
+#🇨🇦 CA
+aws s3 cp data/CAvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=ca/
+aws s3 cp data/CA_category_id.json s3//s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=ca/
+
+#🇩🇪 DE
+aws s3 cp data/DEvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=de/
+aws s3 cp data/DE_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=de/
+
+#🇫🇷 FR
+aws s3 cp data/FRvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=fr/
+aws s3 cp data/FR_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=fr/
+
+#🇬🇧 GB
+aws s3 cp data/GBvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=gb/
+aws s3 cp data/GB_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=gb/
+
+#🇮🇳 IN
+aws s3 cp data/INvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=in/
+aws s3 cp data/IN_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=in/
+
+#🇯🇵 JP
+aws s3 cp data/JPvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=jp/
+aws s3 cp data/JP_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=jp/
+
+#🇰🇷 KR
+aws s3 cp data/KRvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=kr/
+aws s3 cp data/KR_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=kr/
+
+#🇲🇽 MX
+aws s3 cp data/MXvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=mx/
+aws s3 cp data/MX_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=mx/
+
+#🇷🇺 RU
+aws s3 cp data/RUvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=ru/
+aws s3 cp data/RU_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=ru/
+
+#🇺🇸 US
+aws s3 cp data/USvideos.csv s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics/region=us/
+aws s3 cp data/US_category_id.json s3://yt-data-pipeline-bronze-layer/youtube/raw_statistics_ref_data/region=us/
---
+++ scripts/information.md
@@ -0,0 +1,12 @@
+Bronze S3 Bucket Name -- yt-data-pipeline-bronze-layer 
+Silver S3 Bucket Name -- yt-data-pipeline-silver-layer
+Gold   S3 Bucket Name -- yt-data-pipeline-gold-layer
+
+
+script bucket --yt-data-pipeline-scripts-layer
+
+SNS Arn -- arn:aws:sns:us-east-1:092042970260:youtube-data-pipeline-alert:60c95bec-7675-48ee-a573-7170766c8fea
+
+Glue-bronze -- youtube-data-pipeline-bronze
+Glue-silver -- youtube-data-pipeline-silver
+Glue-gold -- youtube-data-pipeline-gold
